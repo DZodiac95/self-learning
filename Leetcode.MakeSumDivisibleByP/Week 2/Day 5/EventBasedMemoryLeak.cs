9e0fec84ee1044a6e886ce1ba2d7f370743e5321
Zodiac <doanthanhnguyen95@gmail.com>
2026-01-21 20:17:01 +0700

    Update

---
+++ Leetcode.MakeSumDivisibleByP/Week 2/Day 5/EventBasedMemoryLeak.cs
@@ -0,0 +1,27 @@
+﻿namespace Studying.Week_2.Day_5
+{
+    public class EventBasedMemoryLeak : IDisposable
+    {
+        private readonly Publisher _publisher;
+        public EventBasedMemoryLeak(Publisher publisher)
+        {
+            //Bad approach.
+            //Publisher((static, singleton, app-wide service)) lives long, subscriber(view, view model, controller) is temporary.
+            //subscriber will leak unless it unsubscribes.
+            publisher.ThresholdReached += OnSomeEvent;
+
+            //Correct approach.
+            _publisher = publisher;
+            _publisher.ThresholdReached += OnSomeEvent;
+        }
+
+        //Correct approach.
+        //Unsubscribe manually.
+        public void Dispose()
+        {
+            _publisher.ThresholdReached -= OnSomeEvent;
+        }
+
+        private void OnSomeEvent(object sender, EventArgs e) { }
+    }
+}
---
+++ Leetcode.MakeSumDivisibleByP/Week 2/Day 5/Publisher.cs
@@ -0,0 +1,32 @@
+﻿namespace Studying.Week_2.Day_5
+{
+    public class Publisher
+    {
+        public event EventHandler<TestEvent> ThresholdReached;
+
+        public void CheckThreshold(int current, int limit)
+        {
+            if (current >= limit)
+            {
+                // 2. Prepare data and raise the event
+                var args = new TestEvent
+                {
+                    Threshold = limit,
+                    TimeReached = DateTime.Now
+                };
+                OnThresholdReached(args);
+            }
+        }
+
+        protected virtual void OnThresholdReached(TestEvent e)
+        {
+            // Null-conditional operator (?.) ensures thread-safety if no subscribers exist
+            ThresholdReached?.Invoke(this, e);
+        }
+    }
+
+    public class TestEvent : EventArgs {
+        public int Threshold { get; set; }
+        public DateTime TimeReached { get; set; }
+    }
+}
